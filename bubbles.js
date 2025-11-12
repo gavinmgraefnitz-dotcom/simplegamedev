6786fbde937bec7b2ef8094e191de4fc1026e8ee
gavinmgraefnitz-dotcom <gavin.m.graefnitz@gmail.com>
2025-11-11 20:28:24 -0500

    Create bubbles.js

---
+++ bubbles.js
@@ -0,0 +1,9 @@
+// src/bubbles.js
+export const bubblePresets = [
+  { label: "🎮 Start Game", type: "input" },
+  { label: "🧍 Spawn Player", type: "default" },
+  { label: "⌨️ On Key Press", type: "default" },
+  { label: "👾 Spawn Enemy", type: "default" },
+  { label: "💥 On Collision", type: "default" },
+  { label: "❤️ Subtract Health", type: "default" },
+];
